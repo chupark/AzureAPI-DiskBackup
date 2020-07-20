5ca9623072b0373733af5e3dffe73e2ff0c93671
chiwoo <qkrcldn12@gmail.com>
2020-07-20 17:32:31 +0900

    add files

--- readme.md
+++ readme.md
@@ -5,7 +5,8 @@ VM 생성, 디스크 스냅샷 생성 등 실행이 오래 걸리는 작업은 H
 따라서 API의 20x로 들어온 Response의 Header.'Azure-AsyncOperation'[0]을 항상 체크하여 작업 상태를 모니터링 해야한다.   
 ![Alt text](https://github.com/chupark/AzureAPI-DiskBackup/raw/master/img/1.%20azure%20api/1.SnapshotSAS_202%20created.png)    
 Azure-AsyncOperation의 데이터는 작업의 상태를 조회할 수 있는 URL이 들어있으므로 해당 URL에 Request를 보내 HTTP Status [200 OK]가 return되는지 확인한다.   
-Disk Snapshot의 SAS URL생성 요청의 경우 최초 응답이 [202 Created]로 return 되고, SAS URL이 응답에 포함되지 않아 당황할 수 있는데 이때는 Azure-AsyncOperation의 URL에 다시 요청을 하면, SAS URL을 받을 수 있다. Status Code가 200이 될 때 까지 반복적으로 요청해야 한다.   
+Disk Snapshot의 SAS URL생성 요청의 경우 최초 응답이 [202 Created]로 return 되고, SAS URL이 응답에 포함되지 않아 당황할 수 있는데 이때는 Azure-AsyncOperation의 URL에 다시 요청을 하면, SAS URL을 받을 수 있다.   
+ Status Code가 200이 될 때 까지 반복적으로 요청해야 한다.   
 ![Alt text](https://raw.githubusercontent.com/chupark/AzureAPI-DiskBackup/master/img/1.%20azure%20api/2.SnapshotSAS_200%20ok.png)    
 <br>
 
---
+++ src/lib/StorageOperation.psm1
@@ -0,0 +1,286 @@
+Class StorageBlob {
+    [String]$storageAccount
+    [String]$date
+    hidden [String]$sasHrKey
+
+    $tableOperations = [StorageBlobOperation].GetEnumNames()
+
+    StorageBlob([String]$storageAccount, [String]$key) {
+        $this.storageAccount = $storageAccount
+        $this.sasHrKey = $key
+    }
+
+    [PSCustomObject]startStoragePageBlobCopyFromURL([String]$url) {
+        $copyDate = [System.DateTime]::UtcNow.ToString("R")
+        $canonicalizedHeaders = "x-ms-copy-source:" + $url       + "`n" + 
+                                "x-ms-date:" + $copyDate                    + "`n" +
+                                "x-ms-version:2018-11-09"               + "`n"
+                                 
+        $StringToSign = "PUT`n`n`n`n" + $CanonicalizedHeaders +"/pcwstoragetable/blob/test2.vhd"
+        
+        $sharedKey = [System.Convert]::FromBase64String($this.sasHrKey)
+        $hasher = New-Object System.Security.Cryptography.HMACSHA256
+        $hasher.Key = $sharedKey
+        $signedSignature = [System.Convert]::ToBase64String($hasher.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($stringToSign)))
+        
+        
+        $copyHeader = @{
+            "x-ms-copy-source"=$url
+            "x-ms-date"=$copyDate
+            "x-ms-version"="2018-11-09"
+            "Authorization"="SharedKeyLite pcwstoragetable:" + $signedSignature
+        }
+        $invokeResult = Invoke-WebRequest -Method Put -Uri "https://pcwstoragetable.blob.core.windows.net/blob/test2.vhd" -Headers $copyHeader
+    
+        return $invokeResult
+    }
+}
+
+enum StorageBlobOperation {
+    startStoragePageBlobCopyFromURL
+}
+
+
+
+
+Class StorageTable {
+    [String]$storageAccount
+    [String]$date
+    hidden [String]$sasHrKey
+
+    $tableOperations = [TableOperation].GetEnumNames()
+
+    StorageTable([String]$storageAccount, [String]$key) {
+        $this.storageAccount = $storageAccount
+        $this.sasHrKey = $key
+    }
+
+    [PSCustomObject]setAuthHeader([String]$operation, [String]$tableName, [String]$query) {
+        $this.date = [System.DateTime]::UtcNow.ToString("R")
+        [PSCustomObject]$header = $null
+        $StringToSign = '';
+        if($this.tableOperations.Contains($operation)) {
+            switch ($operation) {
+                'createTable' { 
+                    $StringToSign = $this.date + "`n" +  
+                                    '/' + $this.storageAccount + '/Tables'
+                    break;
+                }
+                'deleteTable' {
+                    break;
+                }
+                'insertData' {
+                    $StringToSign = $this.date + "`n" +  
+                                    '/' +$this.storageAccount + '/' + $tableName
+                    $header = @{
+                        "Authorization" = "SharedKeyLite " + $this.storageAccount + ":" + $this.makeSignedSignature($StringToSign)
+                        "Content-Type" = "application/json"
+                        "x-ms-date" = $this.date
+                        "x-ms-version" = "2013-08-15"
+                    }
+                    break;
+                }
+                'updateData' {
+                    break;
+                }
+                'deleteDataByPartitionKeyAndRowKey' {
+                    $StringToSign = $this.date + "`n" +  
+                                    '/' +$this.storageAccount + '/' + $tableName + $query
+                    $header = @{
+                        "Authorization" = "SharedKeyLite " + $this.storageAccount + ":" + $this.makeSignedSignature($StringToSign)
+                        "If-Match"="*"
+                        "x-ms-date" = $this.date
+                    }
+                    break;
+                }
+                'deleteDataByTable' {
+                    $StringToSign = $this.date + "`n" +  
+                                    '/' +$this.storageAccount + '/' + $tableName + "()"
+                    break;
+                }
+                'deleteDataByQuery' {
+                    $StringToSign = $this.date + "`n" +  
+                                    '/' +$this.storageAccount + '/' + $tableName + "()"
+                    $header = @{
+                        "Authorization" = "SharedKeyLite " + $this.storageAccount + ":" + $this.makeSignedSignature($StringToSign)
+                        "If-Match"="*"
+                        "x-ms-date" = $this.date
+                    }
+                    break;
+                }
+                'selectByQuery' {
+                    $StringToSign = $this.date + "`n" +  
+                                    '/' +$this.storageAccount + '/' + $tableName + "()"
+                    $header = @{
+                        "Authorization" = "SharedKeyLite " + $this.storageAccount + ":" + $this.makeSignedSignature($StringToSign)
+                        "x-ms-date" = $this.date
+                        "x-ms-version" = "2013-08-15"
+                        "Accept" = "application/json;odata=minimalmetadata"
+                    }
+                    break;
+                }
+                'selectByTable' {
+                    $StringToSign = $this.date + "`n" +  
+                                    '/' +$this.storageAccount + '/' + $tableName + "()"
+                    $header = @{
+                        "Authorization" = "SharedKeyLite " + $this.storageAccount + ":" + $this.makeSignedSignature($StringToSign)
+                        "x-ms-date" = $this.date
+                        "x-ms-version" = "2013-08-15"
+                        "Accept" = "application/json;odata=minimalmetadata"
+                    }
+                    break;
+                }
+                Default {
+                    return $false
+                    break;
+                }
+            }
+        } else {
+            return $false
+        }
+        
+        return $header
+    }
+
+    hidden [String]makeSignedSignature([String]$StringToSign) {
+        $sharedKey = [System.Convert]::FromBase64String($this.sasHrKey)
+        $hasher = New-Object System.Security.Cryptography.HMACSHA256
+        $hasher.Key = $sharedKey
+        $signedSignature = [System.Convert]::ToBase64String($hasher.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($stringToSign)))
+
+        return $signedSignature
+    }
+
+    hidden [PSCustomObject]deleteDataByPartitionKeyAndRowKey([String]$tableName, [String]$PartitionKey, [String]$RowKey) {
+        $header = $this.setAuthHeader('deleteDataByPartitionKeyAndRowKey', $tableName, ("(PartitionKey='" + $PartitionKey + "',RowKey='" + $RowKey + "')"))
+        return Invoke-WebRequest -Uri ("https://"+ $this.storageAccount +".table.core.windows.net/" + $tableName + "(PartitionKey='" + $PartitionKey + "',RowKey='" + $RowKey + "')") `
+                                 -Headers $header `
+                                 -Method Delete 
+    }
+
+    # Select & Remove
+    [PSCustomObject]deleteDataByQuery([String]$tableName, [String]$query) {
+        try{
+            $datas = $this.selectByQuery($tableName, $query) | ConvertFrom-Json
+            foreach ($data in $datas.value) {
+                $this.deleteDataByPartitionKeyAndRowKey($tableName, $data.PartitionKey, $data.RowKey)
+            }
+            return ,$true
+        } catch {
+            return $_
+        }       
+    }
+
+    # Select & Remove
+    [PSCustomObject]deleteDataByTable([String]$tableName) {
+        try{
+            $datas = $this.selectByTable($tableName) | ConvertFrom-Json
+            foreach ($data in $datas.value) {
+                $this.deleteDataByPartitionKeyAndRowKey($tableName, $data.PartitionKey, $data.RowKey)
+            }
+            return ,$true
+        } catch {
+            return $_
+        }
+    }
+
+    [PSCustomObject]insertData([String]$tableName, [String]$jsonBody) {
+        $header = $this.setAuthHeader('insertData', $tableName, '')
+        return Invoke-WebRequest -Uri ("https://"+ $this.storageAccount +".table.core.windows.net/" + $tableName) `
+                                 -Headers $header `
+                                 -Method Post `
+                                 -Body ([System.Text.Encoding]::UTF8.GetBytes($jsonBody))
+    }
+
+    [PSCustomObject]selectByQuery([String]$tableName, [String]$query) {
+        $query = [System.Web.HttpUtility]::UrlPathEncode($query)
+        $header = $this.setAuthHeader("selectByQuery", $tableName, $query)
+        return Invoke-WebRequest -Uri ("https://"+ $this.storageAccount +".table.core.windows.net/" + $tableName + "()?`$filter=" + $query) `
+                                 -Headers $header `
+                                 -Method Get
+    }
+
+    [PSCustomObject]selectByTable([String]$tableName) {
+        $header = $this.setAuthHeader("selectByTable", $tableName, '')
+        return Invoke-WebRequest -Uri ("https://"+ $this.storageAccount +".table.core.windows.net/" + $tableName + "()") `
+                                 -Headers $header `
+                                 -Method Get
+    }
+}
+
+enum TableOperation {
+    createTable
+    deleteTable
+    insertData
+    updateData
+    deleteDataByQuery
+    deleteDataByTable
+    deleteDataByPartitionKeyAndRowKey
+    selectByQuery
+    selectByTable
+}
+
+function StorageTableFromAD() {
+    param(
+        [PSCustomObject]$azAdInfo,
+        [string]$storageAccoutResourceGroup,
+        [string]$storageAccoutName
+    )
+    $saURL = "https://management.azure.com/subscriptions/" + 
+              $azAdInfo.subscription + "/resourceGroups/" + 
+              $storageAccoutResourceGroup + 
+              "/providers/Microsoft.Storage/storageAccounts/" + 
+              $storageAccoutName + 
+              "/listKeys?api-version=2019-06-01"
+    $saHeader = @{
+        "Authorization"="Bearer " + $azAdInfo.token.access_token
+    }              
+    $saData = (Invoke-WebRequest -Method Post `
+                                -Uri $saURL `
+                                -Headers $saHeader).Content | ConvertFrom-Json
+
+    return [StorageTable]::new($storageAccoutName, $saData.keys[0].value)
+}
+
+
+function StorageTable() {
+    param(
+        [String]$storageAccount,
+        [String]$key
+    )
+    
+    return [StorageTable]::new($storageAccount, $key)
+}
+
+
+
+function StorageBlobFromAD() {
+    param(
+        [PSCustomObject]$azAdInfo,
+        [string]$storageAccoutResourceGroup,
+        [string]$storageAccoutName
+    )
+    $saURL = "https://management.azure.com/subscriptions/" + 
+              $azAdInfo.subscription + "/resourceGroups/" + 
+              $storageAccoutResourceGroup + 
+              "/providers/Microsoft.Storage/storageAccounts/" + 
+              $storageAccoutName + 
+              "/listKeys?api-version=2019-06-01"
+    $saHeader = @{
+        "Authorization"="Bearer " + $azAdInfo.token.access_token
+    }              
+    $saData = (Invoke-WebRequest -Method Post `
+                                -Uri $saURL `
+                                -Headers $saHeader).Content | ConvertFrom-Json
+
+    return [StorageBlob]::new($storageAccoutName, $saData.keys[0].value)
+}
+
+function StorageBlob() {
+    param(
+        [String]$storageAccount,
+        [String]$key
+    )
+    
+    return [StorageBlob]::new($storageAccount, $key)
+}
